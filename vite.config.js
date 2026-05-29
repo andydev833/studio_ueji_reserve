26168ae88d90f84c0f01053b063145768a4639f6
andydev833 <sekainoowari.soccer37@gmail.com>
2026-05-29 23:22:01 +0900

    change path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/for_studio_ueji/',
+  base: '/studio_ueji_reserve/',
 })
